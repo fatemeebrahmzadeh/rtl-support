29bb3619619f63535debc4fff1266e230c7bedf6
lady lunafreya <fateme.ebrahimzadeh20@gmail.com>
2022-11-19 23:05:04 +0330

    using basic rules of rtlcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import useRTL, { RTLContext } from './hooks/useRTL';
+import ToggleRTL from './ToggleRTL';
 
 function App() {
+  const [isRTL, setIsRTL] = useRTL();
+  const greetText = isRTL ? '!مرحبا بالعالم' : 'Hello world!';
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <RTLContext.Provider value={{ value: isRTL, setValue: setIsRTL }}>
+      <div className='App'>
+        <div className='App-paragraph'>
+        <h3 className='App-paragraph-1'>{greetText}</h3>
+        <h3 className='App-paragraph-2'>test</h3>
+        </div>
+        <ToggleRTL />
+      </div>
+    </RTLContext.Provider>
   );
 }
 
